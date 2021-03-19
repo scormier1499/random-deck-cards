0190852f3be1e5469a18e5a79f85f1ffcee9cc3d
scormier1499 <sheila_cormier@hotmail.com>
2021-03-19 02:17:02 +0000

    first commit

--- src/app.js
+++ src/app.js
@@ -7,5 +7,14 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let suits = ["hearts", "diamonds", "spades", "clubs"];
+  let values = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K"];
+  let cards = document.querySelector(".card");
+  let cardValues = document.querySelector(".card h1");
+  cards.classList.add(suits[getRndInteger(0, suits.length - 1)]);
+  cardValues.innerHTML = values[getRndInteger(0, values.length - 1)];
 };
+
+function getRndInteger(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
